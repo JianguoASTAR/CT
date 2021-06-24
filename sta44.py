1146917cfe15caab729cb6d43acf852838e1344e
JianguoASTAR <82488839+JianguoASTAR@users.noreply.github.com>
2021-06-24 14:11:27 +0000

    Update sta44.py

--- sta44.py
+++ sta44.py
@@ -1,45 +1,138 @@
-#将DICOM
-import os
-import SimpleITK as sitk
-import numpy as np
-import cv2
+#重命名JPG文件
 import pandas as pd
 import glob
+import os
+import shutil
 
-#批量转换
-def batchCovert(p2p3Path2):
-    # 打开文件,获取路径
-    #0460dc48,BP3R5QO1,219,C:\ct-raw-scan\De-identified scan images\Non-HCC\0460dc48\W5TQRF1G\BP3R5QO1
+
+def delFolder(p2p3Path2):
     df = pd.read_csv(p2p3Path2, header=0, sep=",")
     for index, row in df.iterrows():
         dicompath = df.iloc[index, 3]  # 获取第0行第0列(路径)
-        jpgpath= dicompath+"_jpg"
-        if( not os.path.exists(jpgpath)):
-            os.mkdir(jpgpath)
+        dicompath = dicompath + "_jpg"
+        shutil.rmtree(dicompath)
+        #os.remove(dicompath)
 
-def dicom2jpg(dicompath, jpgpath):
-    if (os.path.exists(dicompath)):
-       fileList = glob.glob(dicompath + "/*")
-       for file in fileList:
-           ds_array = sitk.ReadImage(file)  # 读取dicom文件的相关信息
-           img_array = sitk.GetArrayFromImage(ds_array)  # 获取array
-           # SimpleITK读取的图像数据的坐标顺序为zyx，即从多少张切片到单张切片的宽和高，此处我们读取单张，因此img_array的shape
-           # 类似于 （1，height，width）的形式
-           shape = img_array.shape
-           img_array = np.reshape(img_array, (shape[1], shape[2]))  # 获取array中的height和width
-           high_window = np.max(img_array)
-           low_window = np.min(img_array)
+def renameFile(p2p3Path2):
+    df = pd.read_csv(p2p3Path2, header=0, sep=",")
+    for index, row in df.iterrows():
+        dicompath = df.iloc[index, 3]  # 获取第0行第0列(路径)
+        dicompath = dicompath + "_jpg"
+        #print(dicompath)
+        fileList = glob.glob(dicompath + "/*")
+        for file in fileList:
 
-           lungwin = np.array([low_window * 1., high_window * 1.])
-           newimg = (img_array - lungwin[0]) / (lungwin[1] - lungwin[0])  # 归一化
-           newimg = (newimg * 255).astype('uint8')  # 将像素值扩展到[0,255]
+            filename = os.path.split(file)[-1]  #共8位，I1000000,I2000000, ..., I9000000, I1000001, I1100000, I1200000, ..., I1900000,I2000001,I2100000,
+            filename = filename.split('.')[0]
+            #print(filename)
 
-           filename = os.path.split(file)[-1]
-           jpgpath2 = os.path.join(jpgpath, filename+".jpg")
-           cv2.imwrite(jpgpath2, newimg, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            #先将每逢10的数（尾数为1） I1000001,的命名找出并重新命名
+            if filename[-1]!="0" and len(filename)==8 :
+                newname =int(filename[1:-1])  #取第1-7位的值并转为整数, 将第一位"I"和最后一位"1"去掉
+                lastnumber = int(filename[-1])
+                newname = delZero(str(newname))
+                if lastnumber==1:
+                    newname = int(newname) * 10
+                elif lastnumber == 2:
+                    newname = int(newname) * 100
+                    #newname = "B"+newname
+                elif lastnumber == 3:
+                    #newname = int(newname) * 1000
+                    newname = "C"+newname
+                elif lastnumber == 4:
+                    newname = "D"+newname
+                elif lastnumber == 5:
+                    newname = "E"+newname
+                newname = str(newname)
+                newname = newname.zfill(6)
+                newname = newname+".jpg"
+                print("oldname1:", filename, " newname:", newname)
+                try:
+                    os.rename(file, os.path.join(dicompath, newname))
+                except:
+                    print("false:")
+            elif len(filename)==8:
+                newname = int(filename[1:])  # 取第1-7位的值并转为整数, 将第一位"I"和最后一位"1"去掉
+                newname = delZero(str(newname))
+                newname = newname.zfill(6)
+                newname = str(newname) + ".jpg"
+                print("oldname2:", filename, " newname:", newname)
+                try:
+                    os.rename(file, os.path.join(dicompath, newname))
+                except:
+                    print("false:")
 
-p2p3Path2 = r"Path_P2P3-NonHCC40.csv"
-dicompath=r"G:\SMART\PySMART\data3\DICOM\A001_353"
-jpgpath = r"G:\SMART\PySMART\data3\DICOM\A001_353_Jpg"
+def renameFileDICOM(p2p3Path2):
+    df = pd.read_csv(p2p3Path2, header=0, sep=",")
+    for index, row in df.iterrows():
+        dicompath = df.iloc[index, 3]  # 获取第0行第0列(路径)
+        #dicompath = dicompath + "_jpg"
+        #print(dicompath)
+        fileList = glob.glob(dicompath + "/*")
+        for file in fileList:
 
-dicom2jpg(dicompath, jpgpath)
+            filename = os.path.split(file)[-1]  #共8位，I1000000,I2000000, ..., I9000000, I1000001, I1100000, I1200000, ..., I1900000,I2000001,I2100000,
+            filename = filename.split('.')[0]
+            #print(filename)
+
+            #先将每逢10的数（尾数为1） I1000001,的命名找出并重新命名
+            if filename[-1]!="0" and len(filename)==8 :
+                newname =int(filename[1:-1])  #取第1-7位的值并转为整数, 将第一位"I"和最后一位"1"去掉
+                lastnumber = int(filename[-1])
+                newname = delZero(str(newname))
+                if lastnumber==1:
+                    newname = int(newname) * 10
+                elif lastnumber == 2:
+                    newname = int(newname) * 100
+                    #newname = "B"+newname
+                elif lastnumber == 3:
+                    #newname = int(newname) * 1000
+                    newname = "C"+newname
+                elif lastnumber == 4:
+                    newname = "D"+newname
+                elif lastnumber == 5:
+                    newname = "E"+newname
+                newname = str(newname)
+                newname = newname.zfill(6)
+                newname = newname+".dcm"
+                print("oldname1:", filename, " newname:", newname)
+                try:
+                    os.rename(file, os.path.join(dicompath, newname))
+                except:
+                    print("false:")
+            elif len(filename)==8:
+                newname = int(filename[1:])  # 取第1-7位的值并转为整数, 将第一位"I"和最后一位"1"去掉
+                newname = delZero(str(newname))
+                newname = newname.zfill(6)
+                newname = str(newname) + ".dcm"
+                print("oldname2:", filename, " newname:", newname)
+                try:
+                    os.rename(file, os.path.join(dicompath, newname))
+                except:
+                    print("false:")
+
+
+def renameFileDICOM2(p2p3Path2):
+    df = pd.read_csv(p2p3Path2, header=0, sep=",")
+    for index, row in df.iterrows():
+        dicompath = df.iloc[index, 3]  # 获取第0行第0列(路径)
+        #dicompath = dicompath + "_jpg"
+        #print(dicompath)
+        fileList = glob.glob(dicompath + "/*.jpg")
+        for file in fileList:
+            newname = file[0:-4]+".dcm"
+            print("oldname2:", file, " newname:", newname)
+            os.rename(file, os.path.join(dicompath, newname))
+
+def delZero(name):
+    if name[-1] !="0":
+        return name
+    else:
+        name = name[:-1]
+        return delZero(name)
+
+
+p2p3Path2 = "path2.csv"
+#delFolder(p2p3Path2)
+#renameFile(p2p3Path2)
+renameFileDICOM2(p2p3Path2)
