a13cd62a76386b5df92ad77e27bcfb195c8eb530
JianguoASTAR <82488839+JianguoASTAR@users.noreply.github.com>
2021-06-24 14:11:44 +0000

    Update sta3.py

--- sta3.py
+++ sta3.py
@@ -1,75 +1,49 @@
 import os
-import glob
+import SimpleITK as sitk
+import numpy as np
+import cv2
 import pandas as pd
-import shutil
+import glob
 
-metaDataPath = r"G:\SMART\Datasets\6 CT Scans (batch 2)\MetaData 47"
+#批量转换
+def batchCovert(p2p3Path2):
+    # 打开文件,获取路径
+    #0460dc48,BP3R5QO1,219,C:\ct-raw-scan\De-identified scan images\Non-HCC\0460dc48\W5TQRF1G\BP3R5QO1
+    df = pd.read_csv(p2p3Path2, header=0, sep=",")
+    for index, row in df.iterrows():
+        dicompath = df.iloc[index, 3]  # 获取第0行第0列(路径)
+        jpgpath= dicompath+"_jpg"
+        if( not os.path.exists(jpgpath)):
+            os.mkdir(jpgpath)
+        dicom2jpg(dicompath, jpgpath)
 
-#重命名P1P4
-def reNameP1P4(metaDataPath):
-    # 判断路径是否存在
-    if (os.path.exists(metaDataPath)):
-       fileList = glob.glob(metaDataPath + "/*.csv")
+def dicom2jpg(dicompath, jpgpath):
+    if (os.path.exists(dicompath)):
+       fileList = glob.glob(dicompath + "/*")
        for file in fileList:
-           #通过文件名获取PatientID和序列名称
-           #print(file)  #G:\SMART\Datasets\6 CT Scans (batch 2)\MetaData 47\0460dc48_POST CONTRAST ABD ASIR 2.5MM.csv
            filename = os.path.split(file)[-1]
-           patientID = filename.split('_')[0]
-           seriesName = filename.split('_')[1].replace(".csv","")
-           #print("patientName:", patientID, "  seriesName:", seriesName)
+           if filename !="VERSION":
+               ds_array = sitk.ReadImage(file)  # 读取dicom文件的相关信息
+               img_array = sitk.GetArrayFromImage(ds_array)  # 获取array
+               # SimpleITK读取的图像数据的坐标顺序为zyx，即从多少张切片到单张切片的宽和高，此处我们读取单张，因此img_array的shape
+               # 类似于 （1，height，width）的形式
+               shape = img_array.shape
+               img_array = np.reshape(img_array, (shape[1], shape[2]))  # 获取array中的height和width
+               high_window = np.max(img_array)
+               low_window = np.min(img_array)
 
-           #打开文件,获取路径
-           df = pd.read_csv(file, header=0, sep=",")
-           #columns = df.columns.values.tolist()
-           #print(columns)
-           path = df.iloc[0, 0]  #获取第0行第0列(路径)
-           times = df['0008|0033']
-           times2 = list(set(times)) #去重
-           print("path:",path, " times:", times2)
+               lungwin = np.array([low_window * 1., high_window * 1.])
+               newimg = (img_array - lungwin[0]) / (lungwin[1] - lungwin[0])  # 归一化
+               newimg = (newimg * 255).astype('uint8')  # 将像素值扩展到[0,255]
 
-           #如果只有一种时间，则直接将文件夹名称重命名
-           if(len(times2) == 1):
-               sourePath = os.path.abspath(os.path.join(path, '..'))
-               basePath = os.path.abspath(os.path.join(sourePath, '..'))
-               if(seriesName[0:3] =="PRE"):
-                   targetPath = os.path.join(basePath, 'P1')
-               elif (seriesName[0:3] == "ART"):
-                   targetPath = os.path.join(basePath, 'P2')
-               elif (seriesName[0:3] == "DEL"):
-                   targetPath = os.path.join(basePath, 'P4')
+               filename = os.path.split(file)[-1]
+               jpgpath2 = os.path.join(jpgpath, filename+".jpg")
+               print(jpgpath2)
+               cv2.imwrite(jpgpath2, newimg, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
-               print("sourePath:", sourePath)
-               print("targetPath:", targetPath)
-               os.rename(sourePath, targetPath)
+p2p3Path2 = r"path2.csv"
+batchCovert(p2p3Path2)
+dicompath=r"G:\SMART\PySMART\data3\DICOM\A001_353"
+jpgpath = r"G:\SMART\PySMART\data3\DICOM\A001_353_Jpg"
 
-           #如果有两种时间,则分别将每种时间的文件复制到单独一个文件夹，并重命名
-           if(len(times2) ==2):
-               times2.sort()#先排序
-               #每一种时间
-               sourePath = os.path.abspath(os.path.join(path, '..'))
-               basePath = os.path.abspath(os.path.join(sourePath, '..'))
-
-               newPath1 = os.path.join(basePath, 'P2')
-               newPath2 = os.path.join(basePath, 'P3')
-               #创建新文件夹
-               if not os.path.exists(newPath1):
-                 os.mkdir(newPath1)
-               if not os.path.exists(newPath2):
-                 os.mkdir(newPath2)
-
-               #移动文件
-               #C:\ct2_P2P3\HCC Surveillance\4fbec240\GA1JXMTI\JOGT2EB4/I1000000,165047,165309,165427,165436
-               for index, row in df.iterrows():
-                    path = df.iloc[index, 0]  # 获取第0行第0列(路径)
-                    t= df.iloc[index,'0008|0033']
-                    if(t == times2[0]):   #如果是第一个时间
-                        shutil.move(path, newPath1)
-                        #print(newPath1)
-                    elif(t==times2[1]):  #如果是第二个时间
-                        shutil.move(path, newPath2)
-                        #print(newPath2)
-               #删除空文件夹
-               files = os.listdir(sourePath)
-               if len(files)==0:
-                   os.remove(sourePath)
-reNameP1P4(metaDataPath)
+#dicom2jpg(dicompath, jpgpath)
