778142edecf3280164dfe2ef1105867f91b78a3a
JianguoASTAR <82488839+JianguoASTAR@users.noreply.github.com>
2021-07-06 12:52:24 +0800

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,62 +1,119 @@
-#3 生成NRRD文件
-def saveNRRDBySimpleITK(patientList,scanList, pathList):
-    for i in range(len(pathList)):
-        dicompath= pathList[i].strip() #要去掉两边的空格
-        print(dicompath)
-        series_ids = sitk.ImageSeriesReader.GetGDCMSeriesIDs(dicompath)
-        #print("series ids:", series_ids)
+import glob
+#根据Multiple-Phases的文件清单，对各个患者的Nrrd文件进行重命名，并删除多余文件
+import os
+import csv
 
-        if not series_ids:
-            print("ERROR: given directory dose not a DICOM series.")
-            sys.exit(1)
-        for serie in series_ids:
-            series_file_names = sitk.ImageSeriesReader.GetGDCMSeriesFileNames(dicompath, serie)
-            series_reader = sitk.ImageSeriesReader()
-            series_reader.SetFileNames(series_file_names)
-            image3D = series_reader.Execute()
-            reader = sitk.ImageFileReader()
-            reader.SetFileName(series_file_names[0])
-            reader.ReadImageInformation()
-            size = image3D.GetSize()
-            print("Image size:", size[0], size[1], size[2])
+nrrdPath=r"G:\SMART\Datasets\NRRDFiles_MultiPhases"
+#nrrdPath=r"G:\SMART\Datasets\NRRDFiles_Contrast_MultiPhases"
+csvPath=r"G:\SMART\PySMART\data\MultiplePhasesList.csv"
 
-            image_array = sitk.GetArrayFromImage(image3D)
+#1 获取各个nrrd文件名
+def readNrrdFileName(basePath):
+    pList = list()
+    # 判断路径是否存在
+    if (os.path.exists(basePath)):
+        dirs_type = os.listdir(basePath)
+        #print(dirs_type)
+        # 遍历每一个类别(HCC, Non-HCC)
+        for dir_type in dirs_type:
+            dirs_patient = os.listdir(os.path.join(basePath,dir_type))
+            #print(dirs_patient)
+            # 遍历每个患者
+            for dir_patient in dirs_patient:
+                nrrdPath = os.path.join(basePath, dir_type, dir_patient)
+                print(nrrdPath)
+                fileList = glob.glob(nrrdPath + "/*.nrrd")
+                for f in fileList:
+                    fname = os.path.basename(f)
+                    pList.append([dir_type, dir_patient, f, fname])
+    print(pList)
+    return pList
 
-            # SimpleITK读取的图像数据的坐标顺序为zyx，即从多少张切片到单张切片的宽和高；
-            # 而据SimpleITK Image获取的origin和spacing的坐标顺序则是xyz
+#2 将List列表内容保存为csv文件
+def writeData2CSV(data, filename):
+    f = open(filename,'w')
+    for d in data:
+        f.write(str(d).replace('[','').replace(']','').replace('\'',''))
+        f.write("\n")
+    f.close()
 
-            origin = image3D.GetOrigin()
-            spacing = image3D.GetSpacing()
-            print("origin:", origin, " spacing:", spacing)
-            img2 = sitk.GetImageFromArray(image_array)
-            img2.SetOrigin(origin)
-            s2 = spacing[2] * 1.5
-            spacing2 = (spacing[0], spacing[1], s2)
-            print(s2)
-            img2.SetSpacing(spacing2)
-            print(img2.GetSpacing())
-
-            for k in reader.GetMetaDataKeys():
-                v = reader.GetMetaData(k)
-                img2.SetMetaData(k, v)
-
-            seriedescription = reader.GetMetaData('0008|103e')  # 读取对应的Tag，0002开头的不能读写
-            seriedescription = seriedescription.replace("/","")   #有些描述有"/"符号
-            print(seriedescription)
-            #ImageType = reader.GetMetaData('0008|0008')  # 读取对应的Tag，0002开头的不能读写
-            ##img2.EraseMetaData('0008|0008')  # 删除Tag，更改的话可以不写
-            #img2.SetMetaData('0008|0008', ImageType)  # 更改新dcm的Tag值
-            #SOPClassUID = reader.GetMetaData('0008|0016')
-            #img2.SetMetaData('0008|0016', SOPClassUID)
-
-            #img2.SetMetaData('0008|0018', reader.GetMetaData('0008|0018'))
-
-            #SeriesInstanceUID = reader.GetMetaData('0020|000e')  # 这个Tag改不了，保存时貌似会随机赋值
-            #img2.SetMetaData('0020|000e', SeriesInstanceUID)
-            if (not os.path.samestat('../data3/NRRD/'+patientList[i])):
-                os.mkdir('../data3/NRRD/'+patientList[i])
-
-            sitk.WriteImage(img2, '../data3/NRRD/'+patientList[i] +"/" +seriedescription+".nrrd")
+basePath=r"G:\SMART\PySMART\data3\NRRD"
+csvPath = r"G:\SMART\PySMART\data3\NrrdFileList.csv"
+pList = readNrrdFileName(basePath)
+writeData2CSV(pList, csvPath)
 
 
-saveNRRDBySimpleITK(patientList,scanList, pathList)
+#2 加载Multiple-Phases的文件清单
+def load_Multiple_Phases_List(csvPath):
+    with open(csvPath, 'r') as f:
+        reader = csv.reader(f)
+        result = list(reader)
+        #print(result[1])
+        return result
+
+multPhasesList = load_Multiple_Phases_List(csvPath)
+print(len(multPhasesList))
+
+
+#根据multPhasesList，遍历各患者文件夹并修改名称
+def reNameFileName(multPhasesList, basePath):
+    for f in multPhasesList:
+        srcFile1 = os.path.join(basePath, f[0].strip(), f[1].strip(), f[2].strip()+".nrrd")   #加.strip()是将前后空格去掉
+        srcFile2 = os.path.join(basePath, f[0].strip(), f[1].strip(), f[2].strip()+" .nrrd")  #有些文件名后面有空格
+        print("原始文件名:", srcFile1)
+        dstFile = os.path.join(basePath, f[0].strip(), f[1].strip(), f[3].strip()+"_"+f[1].strip()+".nrrd")
+        print("新文件名:", dstFile)
+        try:
+            if (os.path.exists(srcFile1)):
+                os.rename(srcFile1,dstFile)
+                print('rename file success\r\n')
+            elif (os.path.exists(srcFile2)):
+                os.rename(srcFile2, dstFile)
+                print('rename file success\r\n')
+            else:
+                print("file does not exist")
+        except Exception as e:
+            print(e)
+            print('rename file fail\r\n')
+
+
+#删除多余的文件
+def removeUnuseFiles(basePath):
+    # 判断路径是否存在
+    if (os.path.exists(basePath)):
+        dirs_type = os.listdir(basePath)
+        # 遍历每一个类别
+        for dir_type in dirs_type:
+            typePath = os.path.join(basePath, dir_type)
+            dirs_patient = os.listdir(typePath)
+            # 遍历每一个患者
+            for dir_patient in dirs_patient:
+                patientPath = os.path.join(typePath, dir_patient)
+                files = os.listdir(patientPath)
+                lista = ['P1', 'P2', 'P3', 'P4']
+                for file in files:
+                    fname = file[0:2]  #只获取前两个字符
+                    print(fname)
+                    if fname not in lista:
+                        print("removing ", file)
+                        os.remove(os.path.join(patientPath, file))
+
+#删除当前目录下的空的子文件夹
+def removeEmptyFolder(basePath):
+    folders = list(os.walk(basePath))[1:]
+
+    for folder in folders:
+        # folder example: ('FOLDER/3', [], ['file'])
+        if not folder[2]:
+            os.rmdir(folder[0])
+
+csvPath2 = r"G:\SMART\PySMART\data3\NrrdFileList_v2.csv"
+
+multPhasesList = load_Multiple_Phases_List(csvPath2)
+reNameFileName(multPhasesList, basePath)
+#removeUnuseFiles(basePath)
+
+#basePath = r"c:\ct3_Cleandata\HCC"
+#basePath = r"c:\ct3_Cleandata\HCC Surveillance"
+#basePath = r"c:\ct3_Cleandata\non-HCC"
+removeEmptyFolder(basePath)
